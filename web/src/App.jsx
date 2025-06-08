cb10db6c9aa0b8e43381f9d57ea14eec29c137fc
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2025-06-08 13:19:44 +0530

    added Toast for login page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,118 +1,3 @@
-// import "./App.css";
-// import React from "react";
-// import {
-//   Navigate,
-//   Route,
-//   BrowserRouter as Router,
-//   Routes,
-// } from "react-router-dom";
-// import Login from "./pages/authentication/Login";
-// import Sidebar from "./pages/Sidebar/Sidebar";
-// import Home from "./pages/Home";
-// import Dashborad from "./pages/DashBoard/Dashborad";
-// import Navbar from "./componets/Navbar/Navbar";
-// // import EnquiryForm from "./componets/Navbar/EnquiryForm";
-// import ViewAllLeads from "./pages/DashBoard/ViewAllLeads";
-// // import AuthForm from "./pages/authentication/AuthForm";
-
-// function App() {
-//   return (
-//     <div className="App ">
-//       <Router>
-//         <Routes>
-//           <Route path="/login" element={<Login />} />
-
-//           <Route path="/" element={<Home />}>
-//             <Route path="/dashboard" element={<Dashborad />} />
-//                         {/* <Route path="/dashboard" element={<AuthForm/>} /> */}
-
-//             {/* <Route path="/enquiryForm" element={<EnquiryForm />} /> */}
-//             <Route path="/leads" element={<ViewAllLeads />} />
-
-//           </Route>
-//         </Routes>
-//       </Router>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
-// import Login from "./pages/authentication/Login";
-// import Home from "./pages/Home";
-// import Dashborad from "./pages/DashBoard/Dashborad";
-// import ViewAllLeads from "./pages/DashBoard/ViewAllLeads";
-// // import EnquiryForm from "./pages/DashBoard/EnquiryForm"; // Uncomment when ready
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/login" element={<Login />} />
-
-//         {/* Directly render Home and nested routes under it */}
-//         <Route path="/" element={<Home />}>
-//           <Route path="dashboard" element={<Dashborad />} />
-//           <Route path="leads" element={<ViewAllLeads />} />
-//           {/* <Route path="enquiryForm" element={<EnquiryForm />} /> */}
-//         </Route>
-
-//         {/* Redirect unknown paths to login */}
-//         <Route path="*" element={<Navigate to="/login" />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-// src/App.js
-//  import React from "react";
-//  import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
-//  import Login from "./pages/authentication/Login";
-//  import Home from "./pages/Home";
-//  import Dashborad from "./pages/DashBoard/Dashborad";
-//  import ViewAllLeads from "./pages/DashBoard/ViewAllLeads";
-// //  import EnquiryForm from "./pages/DashBoard/EnquiryForm"; // ✅ Make sure file exists
-// //  import PrivateRoute from "./componets/PrivateRoute/PrivateRoute";
-
-// function App() {
-//    return (
-//      <Router>
-//        <Routes>
-//          <Route path="/login" element={<Login />} />
-        
-//         <Route
-//           path="/"
-//           element={
-//             <PrivateRoute>
-//               <Home />
-//             </PrivateRoute>
-//           }
-//         >
-//           <Route path="dashboard" element={<Dashborad />} />
-//           <Route path="leads" element={<ViewAllLeads />} />
-//            {/* <Route path="enquiryForm" element={<EnquiryForm />} />  */}
-//         </Route>
-
-//         <Route path="*" element={<Navigate to="/login" />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App; 
 
 
 import React from "react";
@@ -124,9 +9,16 @@ import Dashborad from "./pages/DashBoard/Dashborad";
 import ViewAllLeads from "./pages/DashBoard/ViewAllLeads";
 // import EnquiryForm from "./pages/DashBoard/EnquiryForm"; // Uncomment when ready
 
+import { ToastContainer } from "react-toastify"; 
+import "react-toastify/dist/ReactToastify.css";   
+
 function App() {
   return (
     <Router>
+
+      <ToastContainer position="top-right" autoClose={3000} />
+
+
       <Routes>
         <Route path="/login" element={<Login />} />
 
